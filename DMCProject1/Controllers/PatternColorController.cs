f6d6d5850af2af5725769b335aa28fa517e01df5
JudithvdP <judith.v.d.p@hotmail.com>
2021-11-14 16:30:33 +0100

    Added Pattern and PatternColor Controllers with their pages.

---
+++ DMCProject1/Controllers/PatternColorController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DMCProject1.Controllers
+{
+    public class PatternColorController : Controller
+    {
+        // GET: PatternColor
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: PatternColor/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: PatternColor/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: PatternColor/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: PatternColor/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: PatternColor/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: PatternColor/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: PatternColor/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ DMCProject1/Controllers/PatternController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using DMCProject1.DAL;
+using DMCProject1.Models;
+
+namespace DMCProject1.Controllers
+{
+    public class PatternController : Controller
+    {
+        private DmcContext db = new DmcContext();
+
+        // GET: Pattern
+        public ActionResult Index()
+        {
+            return View(db.Patterns.ToList());
+        }
+
+        // GET: Pattern/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Pattern pattern = db.Patterns.Find(id);
+            if (pattern == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pattern);
+        }
+
+        // GET: Pattern/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Pattern/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "PatternId,UserId,Name")] Pattern pattern)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Patterns.Add(pattern);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(pattern);
+        }
+
+        // GET: Pattern/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Pattern pattern = db.Patterns.Find(id);
+            if (pattern == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pattern);
+        }
+
+        // POST: Pattern/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "PatternId,UserId,Name")] Pattern pattern)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(pattern).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(pattern);
+        }
+
+        // GET: Pattern/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Pattern pattern = db.Patterns.Find(id);
+            if (pattern == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pattern);
+        }
+
+        // POST: Pattern/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Pattern pattern = db.Patterns.Find(id);
+            db.Patterns.Remove(pattern);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
