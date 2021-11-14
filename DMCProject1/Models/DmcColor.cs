0c641d666418a398742929aa386fcdcc406cc995
JudithvdP <judith.v.d.p@hotmail.com>
2021-11-14 16:54:10 +0100

    Added extra info to the class

--- DMCProject1/Models/DmcColor.cs
+++ DMCProject1/Models/DmcColor.cs
@@ -10,5 +10,9 @@ namespace DMCProject1.Models
         [Key]
         public int DmcId { get; set; }
         public string Color { get; set; }
+        public int Red { get; set; }
+        public int Green { get; set; }
+        public int Blue { get; set; }
+
     }
 }
