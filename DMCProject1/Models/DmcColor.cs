5d870bd0cd6d084ff87d8831034b1845138af4e3
JudithvdP <judith.v.d.p@hotmail.com>
2021-12-09 17:38:55 +0100

    added DbId for DmcColors because DmcId was overruled

--- DMCProject1/Models/DmcColor.cs
+++ DMCProject1/Models/DmcColor.cs
@@ -8,17 +8,10 @@ namespace DMCProject1.Models
     public class DmcColor
     {
         [Key]
+        public int DbId { get; set; }
         public int DmcId { get; set; }
         public string Name { get; set; }
         public string HexaDecimal { get; set; }
-
-       //public string getHtmlColorString()
-        //{
-
-            //return "rgb(Red, Green, Blue)";
-        //}
-
-        //public static System.Drawing.Color FromArgb(int Red, int Green, int Blue);
     }    
     
     
