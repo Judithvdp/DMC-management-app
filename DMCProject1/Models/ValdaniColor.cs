bedc64062ac33d175845e8bd05ca55d0caf012e6
JudithvdP <judith.v.d.p@hotmail.com>
2022-03-27 21:40:19 +0200

    Added ValdaniColor

---
+++ DMCProject1/Models/ValdaniColor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DMCProject1.Models
+{
+    public class ValdaniColor : AbstractColor
+    {
+        private string id;
+
+        public int DbId { get; set; }
+        public int ValdaniId { get; set; }
+        public override string Name { get; set; }
+        public override string HexaDecimal { get; set; }
+
+        public override string Id
+        {
+            get { return "Valdani." + ValdaniId; }
+            set { id = value; }
+        }
+    }
+}
